50b8d3c5cca533b9540386c64b308f27cb234346
sandymule <sandymule@gmail.com>
2021-06-25 15:57:40 -0700

    updated file

--- implementation/src/hello_world_api_server.h
+++ implementation/src/hello_world_api_server.h
@@ -3,7 +3,7 @@
 #include "edce_node.h"
 #include "rpc/rpcconfig.h"
 #include "rpc/hello_world_api.h"
-#include "xdr/hellow_world_api.h"
+#include "xdr/hello_world_api.h"
 
 #include <xdrpp/arpc.h>
 #include <xdrpp/srpc.h>

a23f81fdf3d47181cd9d876d6fe72073df0c8b87
sandymule <sandymule@gmail.com>
2021-07-08 15:57:56 -0700

    char* to int

--- implementation/src/crypto_utils.cc
+++ implementation/src/crypto_utils.cc
@@ -32,9 +32,9 @@ public:
 
 			// -------DELETE THIS CODE------------
 			std::cout << "SAM CHECK" << std::endl;
-			std::printf(block[i].transaction);
-			std::printf(block[i].signature);
-			std::printf(*pk_opt);
+			std::printf("%lu\n", block[i].transaction);
+			std::printf("%lu\n", block[i].signature);
+			std::printf("%lu\n", pks[i]);
 			// -----------------------------------
 
 			if (!sig_check(block[i].transaction, block[i].signature, pks[i])) {
@@ -103,9 +103,9 @@ public:
 
 			// -------DELETE THIS CODE------------
 			std::cout << "ORIGINAL CHECK" << std::endl;
-			std::printf(block[i].transaction);
-			std::printf(block[i].signature);
-			std::printf(*pk_opt);
+			std::printf("%lu\n", block[i].transaction);
+			std::printf("%lu\n", block[i].signature);
+			std::printf("%lu\n", *pk_opt);
 			// -----------------------------------
 
 			if (!sig_check(block[i].transaction, block[i].signature, *pk_opt)) {

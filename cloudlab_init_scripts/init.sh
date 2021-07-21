c61d5e78cbbea57cd2df84c98c2b0e9c67014996
sandymule <sandymule@gmail.com>
2021-07-21 14:30:21 -0700

    init script

--- cloudlab_init_scripts/init.sh
+++ cloudlab_init_scripts/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ../implementation
+cd /users/samwwong/xdrpp-hello-world/implementation
 
 sudo ./autogen.sh
 
@@ -8,8 +8,20 @@ sudo ./configure
 
 sudo make || true
 
+cd /users/samwwong/xdrpp-hello-world/implementation/src
+
+sudo make synthetic_data_gen
+
+sudo make singlenode_experiment
+
+sudo make singlenode_validator
+
+sudo ./synthetic_data_gen default_params.yaml synthetic_data_generator/synthetic_data_params.yaml basic_allvalid
+
+sudo make signature_shard_controller
+
+sudo make signature_check_server_main
+
 cd /users/samwwong/xdrpp-hello-world/
 
 chmod 777 -R .
-
-echo "HELLO WORLD"

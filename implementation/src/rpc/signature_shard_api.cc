b9d4259fcf434c87dbc18b7cd807246ac986dba7
sandymule <sandymule@gmail.com>
2021-08-16 14:06:18 -0700

    filter

--- implementation/src/rpc/signature_shard_api.cc
+++ implementation/src/rpc/signature_shard_api.cc
@@ -147,16 +147,16 @@ void SignatureShardV1_server::filter_txs(const SignedTransactionWithPKList& tx_w
                 }
             }
         });
-
+/*
     for (auto it = tx_with_pks.begin(); it != tx_with_pks.end(); it++) {
         if (it->signedTransaction.transaction.metadata.sourceAccount != 0) {
             std::cout << it->pk << std::endl;
         }
-    }
+    }*/
 
-    //std::copy_if(tx_with_pks.begin(), tx_with_pks.end(), std::back_inserter(filtered_tx_with_pk_list),
-    //    [](auto val) {return val.pk != 0;});
-    filtered_tx_with_pk_list.insert(filtered_tx_with_pk_list.end(), tx_with_pks.begin(), tx_with_pks.end());
+    std::copy_if(tx_with_pks.begin(), tx_with_pks.end(), std::back_inserter(filtered_tx_with_pk_list),
+        [](auto val) {return val.signedTransaction.transaction.metadata.sourceAccount != 0;});
+    //filtered_tx_with_pk_list.insert(filtered_tx_with_pk_list.end(), tx_with_pks.begin(), tx_with_pks.end());
 }
 
 

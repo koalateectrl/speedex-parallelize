963d396291f055821af7befaf23dbed1dfff7428
sandymule <sandymule@gmail.com>
2021-09-16 10:57:15 -0700

    changed str to bitset

--- implementation/src/test_multiset_hash_speed.cc
+++ implementation/src/test_multiset_hash_speed.cc
@@ -26,31 +26,26 @@
 #define SQRT_OUTPUT_BITS 16
 
 
-void create_str_vec(std::vector<std::string> &str_vec, const std::string& message, uint64_t n) {
-    unsigned char out[crypto_hash_sha256_BYTES];
-    uint64_t msg_size = message.size();
+void create_str_vec(std::vector<std::bitset<SQRT_OUTPUT_BITS>> &bitset_vec, const std::string& message, uint64_t n) {
     const unsigned char* msg = reinterpret_cast<const unsigned char *> (message.c_str());
-    crypto_hash_sha256(out, msg, msg_size);
+    unsigned char out[crypto_hash_sha256_BYTES];
+    crypto_hash_sha256(out, msg, message.size());
 
     std::string concat;
-
     for (size_t i = 0; i < sizeof(out)/sizeof(out[0]); i++) {
         concat.append(std::bitset<8>(out[i]).to_string());
     }
 
     for (size_t i = 0; i < SQRT_OUTPUT_BITS; i++) {
-        std::string substr = concat.substr(i * SQRT_OUTPUT_BITS, SQRT_OUTPUT_BITS);
-        str_vec.push_back(substr);
+        bitset_vec.push_back(std::bitset<SQRT_OUTPUT_BITS>(concat.substr(i * SQRT_OUTPUT_BITS, SQRT_OUTPUT_BITS)));
     }
 }
 
-
-void sum_two_vecs(std::vector<std::string> &vec_sum, const std::vector<std::string> &vec_to_add, uint64_t n) {
-
-    for (size_t i = 0; i < vec_sum.size(); i++) {
-        unsigned long long orig_sum = std::bitset<SQRT_OUTPUT_BITS>(vec_sum[i]).to_ullong() +
-            std::bitset<SQRT_OUTPUT_BITS>(vec_to_add[i]).to_ullong();
-        vec_sum[i] = std::bitset<SQRT_OUTPUT_BITS>(orig_sum % n).to_string();
+void sum_two_vecs(std::vector<std::bitset<SQRT_OUTPUT_BITS>> &bitset_sum, 
+    std::vector<std::bitset<SQRT_OUTPUT_BITS>> &vec_to_add, uint64_t n) {
+    for (size_t i = 0; i < bitset_sum.size(); i++) {
+        bitset_sum[i] = std::bitset<SQRT_OUTPUT_BITS>((std::bitset<SQRT_OUTPUT_BITS>(bitset_sum[i]).to_ullong() + 
+            std::bitset<SQRT_OUTPUT_BITS>(vec_to_add[i]).to_ullong()) % n);
     }
 }
 
@@ -78,27 +73,26 @@ int main(int argc, char const *argv[]) {
 
     // create vector of strings
     for (size_t i = 0; i < num_test_accounts; i++) {
-        std::string int_str = std::to_string(i);
-        const unsigned char* msg = reinterpret_cast<const unsigned char *> (int_str.c_str());
-        unsigned char out[crypto_hash_sha256_BYTES];
-        crypto_hash_sha256(out, msg, int_str.size());
-        msg_vec.push_back(std::string(out, out + sizeof(out)/sizeof(out[0])));
+        msg_vec.push_back(std::string {i});
     }
 
     float create_res = edce::measure_time(create_timestamp);
 
     std::cout << "Create " << num_test_accounts << " 256 bit inputs in " << create_res << std::endl;
 
-    std::vector<std::string> summed_vec;
+    std::vector<std::bitset<SQRT_OUTPUT_BITS>> summed_vec;
+    for (size_t i = 0; i < SQRT_OUTPUT_BITS; i++) {
+        summed_vec.push_back(std::bitset<SQRT_OUTPUT_BITS>{0});
+    }
     create_str_vec(summed_vec, msg_vec[0], n);
 
     auto hash_timestamp = edce::init_time_measurement();
 
     // hashing + summing 
-    for (size_t i = 0; i < msg_vec.size() - 1; i++) {
-        std::vector<std::string> str_vec;
-        create_str_vec(str_vec, msg_vec[i + 1], n);
-        sum_two_vecs(summed_vec, str_vec, n);
+    for (size_t i = 0; i < msg_vec.size(); i++) {
+        std::vector<std::bitset<SQRT_OUTPUT_BITS>> bitset_vec;
+        create_str_vec(bitset_vec, msg_vec[i], n);
+        sum_two_vecs(summed_vec, bitset_vec, n);
     }
 
     float hash_res = edce::measure_time(hash_timestamp);

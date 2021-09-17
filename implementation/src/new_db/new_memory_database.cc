10dd13a3ec7f0fedbed78818ccb045ac03722cbe
sandymule <sandymule@gmail.com>
2021-09-17 16:45:19 -0700

    edits to hashing

--- implementation/src/new_db/new_memory_database.cc
+++ implementation/src/new_db/new_memory_database.cc
@@ -5,18 +5,18 @@ std::vector<std::bitset<SQRT_OUTPUT_BITS>> NewMemoryDatabase::get_hash() {
     return hash_;
 }
 
-void NewMemoryDatabase::calculate_hash() {
-    uint64_t n = pow(2, SQRT_OUTPUT_BITS);
+void NewMemoryDatabase::calculate_hash_from_scratch() {
+    clear_hash();
     for (auto it = database_.begin(); it != database_.end(); it++) {
         std::string message = std::string{static_cast<char>(it->first)} + it->second.toString();
         std::vector<std::bitset<SQRT_OUTPUT_BITS>> bitset_vec;
-        create_str_vec(bitset_vec, message, n);
-        sum_two_vecs(hash_, bitset_vec, n);
+        create_bitvec(bitset_vec, message);
+        add_bitvec(hash_, bitset_vec);
     }
 }
 
-void NewMemoryDatabase::create_str_vec(std::vector<std::bitset<SQRT_OUTPUT_BITS>> &bitset_vec, 
-    const std::string& message, uint64_t n) {
+void NewMemoryDatabase::create_bitvec(std::vector<std::bitset<SQRT_OUTPUT_BITS>> &bitset_vec, 
+    const std::string& message) {
     
     const unsigned char* msg = reinterpret_cast<const unsigned char *> (message.c_str());
     unsigned char out[crypto_hash_sha256_BYTES];
@@ -32,11 +32,19 @@ void NewMemoryDatabase::create_str_vec(std::vector<std::bitset<SQRT_OUTPUT_BITS>
     }
 }
 
-void NewMemoryDatabase::sum_two_vecs(std::vector<std::bitset<SQRT_OUTPUT_BITS>> &bitset_sum, 
-    std::vector<std::bitset<SQRT_OUTPUT_BITS>> &vec_to_add, uint64_t n) {
+void NewMemoryDatabase::add_bitvec(std::vector<std::bitset<SQRT_OUTPUT_BITS>> &bitset_sum, 
+    std::vector<std::bitset<SQRT_OUTPUT_BITS>> &vec_to_add) {
     for (size_t i = 0; i < bitset_sum.size(); i++) {
         bitset_sum[i] = std::bitset<SQRT_OUTPUT_BITS>((std::bitset<SQRT_OUTPUT_BITS>(bitset_sum[i]).to_ullong() + 
-            std::bitset<SQRT_OUTPUT_BITS>(vec_to_add[i]).to_ullong()) % n);
+            std::bitset<SQRT_OUTPUT_BITS>(vec_to_add[i]).to_ullong()) % hash_n_);
+    }
+}
+
+void NewMemoryDatabase::subtract_bitvec(std::vector<std::bitset<SQRT_OUTPUT_BITS>> &bitset_sum, 
+    std::vector<std::bitset<SQRT_OUTPUT_BITS>> &vec_to_subtract) {
+    for (size_t i = 0; i < bitset_sum.size(); i++) {
+        bitset_sum[i] = std::bitset<SQRT_OUTPUT_BITS>((std::bitset<SQRT_OUTPUT_BITS>(bitset_sum[i]).to_ullong() - 
+            std::bitset<SQRT_OUTPUT_BITS>(vec_to_subtract[i]).to_ullong()) % hash_n_);
     }
 }
 
@@ -71,6 +79,11 @@ void NewMemoryDatabase::account_creation(const AccountID account_id) {
     }
     std::lock_guard<std::shared_mutex> lock(mutex_);
     database_.insert(std::pair<AccountID, NewUserAccount>{account_id, NewUserAccount{}});
+
+    std::string message = std::string{static_cast<char>(account_id)} + get_account(account_id).toString();
+    std::vector<std::bitset<SQRT_OUTPUT_BITS>> bitset_vec;
+    create_bitvec(bitset_vec, message);
+    add_bitvec(hash_, bitset_vec);
 }
 
 
@@ -84,13 +97,24 @@ void NewMemoryDatabase::update_balance(const AccountID account_id, const std::st
         throw std::runtime_error("Balance is negative");
     }
 
+    std::lock_guard<std::shared_mutex> lock(mutex_);
     NewUserAccount& account = find_account(account_id);
+    std::string message = std::string{static_cast<char>(account_id)} + account.toString();
+    std::vector<std::bitset<SQRT_OUTPUT_BITS>> bitset_vec;
+    create_bitvec(bitset_vec, message);
+    subtract_bitvec(hash_, bitset_vec);
+
     auto token_it = account.token_map_.find(token_name);
     if (token_it == account.token_map_.end()) {
         account.token_map_.insert(std::pair<std::string, int64_t>{token_name, amount});
     } else {
         token_it->second += amount;
     }
+
+    std::string message_new = std::string{static_cast<char>(account_id)} + get_account(account_id).toString();
+    std::vector<std::bitset<SQRT_OUTPUT_BITS>> bitset_vec_new;
+    create_bitvec(bitset_vec_new, message_new);
+    add_bitvec(hash_, bitset_vec_new);
 }
 
 void NewMemoryDatabase::remove_account(const AccountID account_id) {
@@ -98,20 +122,31 @@ void NewMemoryDatabase::remove_account(const AccountID account_id) {
         throw std::runtime_error("Invalid db access - no account");
     }
 
+    std::lock_guard<std::shared_mutex> lock(mutex_);
+    std::string message = std::string{static_cast<char>(account_id)} + get_account(account_id).toString();
+    std::vector<std::bitset<SQRT_OUTPUT_BITS>> bitset_vec;
+    create_bitvec(bitset_vec, message);
+    subtract_bitvec(hash_, bitset_vec);
+
     auto remove_it = database_.find(account_id);
     database_.erase(remove_it);
 }
 
 void NewMemoryDatabase::clear_db() {
+    std::lock_guard<std::shared_mutex> lock(mutex_);
     database_.clear();
-    hash_.clear();
-    for (size_t i = 0; i < SQRT_OUTPUT_BITS; i++) {
-        hash_.push_back(std::bitset<SQRT_OUTPUT_BITS>{0});
-    }
+    clear_hash();
 }
 
 size_t NewMemoryDatabase::size() const {
     return database_.size();
+}
+
+void NewMemoryDatabase::clear_hash() {
+    hash_.clear();
+    for (size_t i = 0; i < SQRT_OUTPUT_BITS; i++) {
+        hash_.push_back(std::bitset<SQRT_OUTPUT_BITS>{0});
+    }
 }
 
 NewUserAccount& NewMemoryDatabase::find_account(const AccountID account_id) {
@@ -136,21 +171,29 @@ int main() {
     NewMemoryDatabase db;
     db.account_creation(1);
     db.update_balance(1, "ADA", 5);
+
+    std::vector<std::bitset<SQRT_OUTPUT_BITS>> before = db.get_hash();
+    
     db.account_creation(2);
     db.update_balance(2, "ATOM", 10);
-    std::cout << db.get_token_balance(1, "ADA") << std::endl;
-    std::cout << db.get_account(1).toString() << std::endl;
-    std::vector<std::bitset<SQRT_OUTPUT_BITS>> before = db.get_hash();
-    db.calculate_hash();
+
     std::vector<std::bitset<SQRT_OUTPUT_BITS>> after = db.get_hash();
+    
+    db.remove_account(2);
+    std::vector<std::bitset<SQRT_OUTPUT_BITS>> after_remove = db.get_hash();
 
-    for (size_t i = 0; i < SQRT_OUTPUT_BITS; i++) {
-        std::cout << before[i] << std::endl;
-    }
+    std::cout << before[0] << std::endl;
+    std::cout << "---------------------------" << std::endl;
+    
+    std::cout << after[0] << std::endl;
 
-    for (size_t i = 0; i < SQRT_OUTPUT_BITS; i++) {
-        std::cout << after[i] << std::endl;
-    }
+    std::cout << "---------------------------" << std::endl;
+
+    std::cout << after_remove[0] << std::endl;
+
+    db.calculate_hash_from_scratch();
+    std::cout << db.get_hash()[0] << std::endl;
+    
 
 }
 
--- implementation/src/new_db/new_memory_database.h
+++ implementation/src/new_db/new_memory_database.h
@@ -14,12 +14,7 @@
 class NewMemoryDatabase {
 public:
     std::vector<std::bitset<SQRT_OUTPUT_BITS>> get_hash();
-    void calculate_hash();
-    void create_str_vec(std::vector<std::bitset<SQRT_OUTPUT_BITS>> &bitset_vec, 
-        const std::string& message, uint64_t n);
-    void sum_two_vecs(std::vector<std::bitset<SQRT_OUTPUT_BITS>> &bitset_sum, 
-        std::vector<std::bitset<SQRT_OUTPUT_BITS>> &vec_to_add, uint64_t n);
-
+    void calculate_hash_from_scratch();
     NewUserAccount get_account(const AccountID account_id);
     std::unordered_map<TokenName, int64_t> get_all_tokens(const AccountID account_id);
     int64_t get_token_balance(const AccountID account_id, const std::string& token_name);
@@ -38,10 +33,19 @@ public:
 
 private:
     std::vector<std::bitset<SQRT_OUTPUT_BITS>> hash_;
+    uint64_t hash_n_;
     std::shared_mutex mutex_;
     std::unordered_map<AccountID, NewUserAccount> database_;
 
+    void clear_hash();
     NewUserAccount& find_account(const AccountID account_id);
+    
+    void create_bitvec(std::vector<std::bitset<SQRT_OUTPUT_BITS>> &bitset_vec, 
+        const std::string& message);
+    void add_bitvec(std::vector<std::bitset<SQRT_OUTPUT_BITS>> &bitset_sum, 
+        std::vector<std::bitset<SQRT_OUTPUT_BITS>> &vec_to_add);
+    void subtract_bitvec(std::vector<std::bitset<SQRT_OUTPUT_BITS>> &bitset_sum, 
+        std::vector<std::bitset<SQRT_OUTPUT_BITS>> &vec_to_subtract);
 
     
 };
